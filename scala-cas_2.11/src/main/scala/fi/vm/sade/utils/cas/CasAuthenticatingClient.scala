a24390fbc92984b9be5559b102d7a702a285ee69
Juha Paananen <juha.paananen@gmail.com>
2016-02-18 17:08:50 +0200

    partial re-implementation with the aim to simplify and improve code readability

--- scala-cas_2.11/src/main/scala/fi/vm/sade/utils/cas/CasAuthenticatingClient.scala
+++ scala-cas_2.11/src/main/scala/fi/vm/sade/utils/cas/CasAuthenticatingClient.scala
@@ -1,37 +1,65 @@
 package fi.vm.sade.utils.cas
 
+import fi.vm.sade.utils.cas.CasClient._
 import org.http4s._
-import org.http4s.client.{DisposableResponse, Client}
+import org.http4s.client.{Client, DisposableResponse}
 import org.http4s.headers.Location
+
 import scalaz.concurrent.Task
-import scalaz.stream._
-import CasClient._
+
 
 /**
- *  HTTP client implementation that handles CAS authentication automatically
+ *  HTTP client implementation that handles CAS authentication automatically. Sessions are maintained by keeping
+ *  a central cache of JSESSIONids per service url. If a JSESSIONID is not found for requested service, it is obtained using
+ *  CasClient. Stale sessions are detected and refreshed automatically.
  */
-object CasAuthenticatingClient {
-  def apply(casClient: CasClient, casParams: CasParams, serviceClient: Client): Client = {
-    val paramSource: Process[Task, CasParams] = Process(casParams).toSource
-    val sessions = paramSource through casClient.casSessionChannel
-    val sessionRefreshProcess = paramSource through casClient.sessionRefreshChannel
-    val requestChannel = channel.lift[Task, Request, DisposableResponse]((req: Request) => serviceClient.open(req)).contramap[(Request, JSessionId)] {
-      case (req: Request, session: JSessionId) => req.putHeaders(headers.Cookie(Cookie("JSESSIONID", session)))
+object CasAuthenticatingClient extends Logging {
+  def apply(casClient: CasClient, casParams: CasParams, serviceClient: Client): Client = new CasAuthenticatingClient(casClient, casParams, serviceClient).httpClient
+}
+
+class CasAuthenticatingClient(casClient: CasClient, casParams: CasParams, serviceClient: Client) extends Logging {
+  lazy val httpClient = Client(
+    open = Service.lift(open _),
+    shutdown = serviceClient.shutdown
+  )
+
+  private var sessions: collection.mutable.Map[CasParams, JSessionId] = collection.mutable.Map.empty
+
+  private def open(req: Request): Task[DisposableResponse] = {
+    openWithCasSession(getCasSession(casParams), req).flatMap {
+      case resp if sessionExpired(resp.response) =>
+        logger.debug("Session for " + casParams + " expired")
+        openWithCasSession(refreshSession(casParams), req)
+      case resp =>
+        Task.now(resp)
     }
+  }
 
-    def sessionExpired(resp: Response): Boolean = resp.status.code == Status.Found.code && resp.headers.get(Location).exists(_.value.contains("/cas/login"))
-
-    def open(req: Request): Task[DisposableResponse] = {
-      def requestProcess(req: Request): Process[Task, DisposableResponse] = Process(req).toSource zip (sessions) through requestChannel flatMap {
-        case resp if sessionExpired(resp.response) => sessionRefreshProcess.drain ++ requestProcess(req)
-        case resp => Process(resp).toSource
-      }
-      requestProcess(req).runLast.map(_.getOrElse(throw new Exception("FAILURE!!!!")))
+  private def openWithCasSession(sessionIdTask: Task[JSessionId], request: Request): Task[DisposableResponse] = {
+    sessionIdTask.flatMap { jsessionid =>
+      serviceClient.open(request.putHeaders(headers.Cookie(Cookie("JSESSIONID", jsessionid))))
     }
+  }
 
-    Client(
-      open = Service.lift(open _),
-      shutdown = serviceClient.shutdown
-    )
+  private def sessionExpired(resp: Response): Boolean = {
+    resp.status.code == Status.Found.code && resp.headers.get(Location).exists(_.value.contains("/cas/login"))
+  }
+
+  private def getCasSession(params: CasParams): Task[JSessionId] = {
+    synchronized(sessions.get(params)) match {
+      case None =>
+        logger.debug("No existing jsessionid found for " + params + ", creating new")
+        refreshSession(params)
+      case Some(session) =>
+        Task.now(session)
+    }
+  }
+
+  private def refreshSession(params: CasParams): Task[JSessionId] = {
+    casClient.fetchCasSession(params).map { session =>
+      logger.debug("Storing new jsessionid for " + params)
+      synchronized(sessions.put(params, session))
+      session
+    }
   }
 }
