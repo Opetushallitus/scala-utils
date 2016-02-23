f35a0d9a39396ab5670a55ef6b87f9dcab67e4ad
Mikko Apo <mikko.apo@reaktor.fi>
2016-02-23 13:19:51 +0200

    properties: Querystring can be defined with Map[String, List]

--- scala-properties_2.11/src/main/scala/fi.vm.sade.scalaproperties/OphProperties.scala
+++ scala-properties_2.11/src/main/scala/fi.vm.sade.scalaproperties/OphProperties.scala
@@ -1,5 +1,7 @@
 package fi.vm.sade.scalaproperties
 
+import java.util
+
 import scala.collection.Map
 
 /**
@@ -9,30 +11,45 @@ class OphProperties extends fi.vm.sade.properties.OphProperties {
   private val excludeCCFields = List("$outer")
   private def caseClassToMap(cc: Product) = {
     val declaredFields = cc.getClass.getDeclaredFields.toList.filter( f => !excludeCCFields.contains(f.getName))
-    (Map[String, Any]() /: declaredFields) {(a, f) =>
+    (Map[AnyRef, AnyRef]() /: declaredFields) {(a, f) =>
       f.setAccessible(true)
       a + (f.getName -> f.get(cc))
     }
   }
 
-  private def removeOption(map: Map[String, Any]) = {
+  private def removeOption(map: Map[AnyRef, AnyRef]) = {
     for( (k,v) <- map if v != None)
       yield (k, v match {
-        case Some(option)  => option
+        case Some(option:AnyRef) => option
         case _ => v
       } )
   }
 
-  private def toJavaMap(map: Map[String, Any]) = collection.JavaConversions.mapAsJavaMap(removeOption(map))
+  private def toJavaMap(map: Map[AnyRef, AnyRef]) = {
+    val dest = new util.LinkedHashMap[AnyRef, AnyRef](map.size)
+    val option: Map[AnyRef, AnyRef] = removeOption(map)
+    option.foreach{ case (k,v) => dest.put(k,convertToJava(v))}
+    dest
+  }
+
+  private def toJavaList(seq: Seq[AnyRef]) = {
+    val dest = new util.ArrayList[AnyRef](seq.size)
+    seq.foreach{ case (u) => dest.add(convertToJava(u))}
+    dest
+  }
+
+  private def convertToJava(o:AnyRef): AnyRef = o match {
+    case seq: Seq[AnyRef] =>
+      toJavaList(seq.asInstanceOf[Seq[AnyRef]])
+    case map: Map[AnyRef, AnyRef] =>
+      toJavaMap(map.asInstanceOf[Map[AnyRef, AnyRef]])
+    case cc: Product =>
+      toJavaMap(caseClassToMap(cc))
+    case _ =>
+      o
+  }
 
   override def convertParams(params: AnyRef*): Array[AnyRef] = {
-    params.map {
-      case map: Map[String, Any] =>
-        toJavaMap(map.asInstanceOf[Map[String, Any]])
-      case cc: Product =>
-        toJavaMap(caseClassToMap(cc))
-      case o =>
-        o
-    }.toArray
+    params.map(convertToJava).toArray
   }
 }
--- scala-properties_2.11/src/test/scala/fi.vm.sade.scalaproperties/OphPropertiesSpec.scala
+++ scala-properties_2.11/src/test/scala/fi.vm.sade.scalaproperties/OphPropertiesSpec.scala
@@ -12,13 +12,14 @@ class OphPropertiesSpec extends FunSuite {
   test("scala conversions") {
     case class Pow(param: String, b: Option[String] = None)
     val ctx: OphProperties = new OphProperties()
-    val props =  new Properties()
+    val props = new Properties()
     ctx.ophProperties = props
     props.setProperty("a.b", "1")
     props.setProperty("b.b", "$1 $param")
     assert(ctx.url("a.b") == "1")
     assert(ctx.url("b.b", "c", Pow("2")) == "c 2")
-    assert(ctx.url("a.b", Pow("2",Some("POW!"))) == "1?param=2&b=POW!")
-    assert(ctx.url("b.b", Pow("2",Some("POW!"))) == "$1 2?b=POW!")
+    assert(ctx.url("a.b", Pow("2", Some("POW!"))) == "1?param=2&b=POW!")
+    assert(ctx.url("b.b", Pow("2", Some("POW!"))) == "$1 2?b=POW!")
+    assert(ctx.url("b.b", Map("a" -> List("a", "b"))) == "$1 $param?a=a&a=b" )
   }
 }
