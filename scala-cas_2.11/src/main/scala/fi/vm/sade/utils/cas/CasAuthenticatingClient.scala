d973dda6312d3c5fe01e8a3461c45f84544d2e79
Mikko Apo <mikko.apo@reaktor.fi>
2016-03-21 16:15:16 +0200

    CasAuthenticatingClient sends CSRF and clientSubSystemCode headers

--- scala-cas_2.11/src/main/scala/fi/vm/sade/utils/cas/CasAuthenticatingClient.scala
+++ scala-cas_2.11/src/main/scala/fi/vm/sade/utils/cas/CasAuthenticatingClient.scala
@@ -1,16 +1,20 @@
 package fi.vm.sade.utils.cas
 
-import org.http4s._
+import org.http4s.{headers, _}
 import org.http4s.client.Client
 import org.http4s.headers.Location
+
 import scalaz.concurrent.Task
 import scalaz.stream._
 import CasClient._
 
+import scala.collection.mutable.ListBuffer
+
 /**
  *  HTTP client implementation that handles CAS authentication automatically
  */
-class CasAuthenticatingClient(casClient: CasClient, casParams: CasParams, serviceClient: Client) extends Client {
+class CasAuthenticatingClient(casClient: CasClient, casParams: CasParams, serviceClient: Client, clientSubSystemCode: Option[String] = None) extends Client {
+
   override def prepare(req: Request): Task[Response] = {
     def requestProcess(req: Request): Process[Task, Response] = Process(req).toSource zip (sessions) through requestChannel flatMap { case resp if sessionExpired(resp) => sessionRefreshProcess.drain ++ requestProcess(req)
     case resp => Process(resp).toSource
@@ -24,7 +28,16 @@ class CasAuthenticatingClient(casClient: CasClient, casParams: CasParams, servic
   private val sessions = paramSource through casClient.casSessionChannel
   private val sessionRefreshProcess = paramSource through casClient.sessionRefreshChannel
   private val requestChannel = channel.lift[Task, Request, Response]((req: Request) => serviceClient.prepare(req)).contramap[(Request, JSessionId)] {
-    case (req: Request, session: JSessionId) => req.putHeaders(headers.Cookie(Cookie("JSESSIONID", session)))
+    case (req: Request, session: JSessionId) => addHeaders(req, session)
+  }
+
+  def addHeaders(req: Request, session: JSessionId): Request = {
+    val csrf = "CasAuthenticatingClient"
+    var list: ListBuffer[Header] = ListBuffer(headers.Cookie(Cookie("JSESSIONID", session), Cookie("CSRF",csrf)), Header("CSRF", csrf))
+    if(clientSubSystemCode.isDefined) {
+      list += Header("clientSubSystemCode", clientSubSystemCode.get)
+    }
+    req.putHeaders(list:_*)
   }
 
   private def sessionExpired(resp: Response): Boolean =
