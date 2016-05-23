b9c335ed7d4109e7ece48f1dbe11459593613076
Jani Rahkola <jani.rahkola@reaktor.com>
2016-05-23 18:16:49 +0300

    Upgrade http4s and scalaz dependencies

--- scala-cas_2.11/src/main/scala/fi/vm/sade/utils/cas/CasParams.scala
+++ scala-cas_2.11/src/main/scala/fi/vm/sade/utils/cas/CasParams.scala
@@ -1,7 +1,7 @@
 package fi.vm.sade.utils.cas
 
+import org.http4s.Uri
 import org.http4s.dsl._
-import org.http4s.{ParseException, Uri}
 
 case class CasUser(username: String, password: String)
 
@@ -15,7 +15,7 @@ object CasParams {
 
   def apply(service:String, username: String, password: String):CasParams =
     Uri.fromString(ensureTailingSlash(service)).fold((e) =>
-      throw ParseException(e), (service: Uri) =>
+      throw e, (service: Uri) =>
       CasParams.apply(CasService(resolve(service, uri("j_spring_cas_security_check"))),
         CasUser(username, password)))
   private def ensureTailingSlash(service:String) = service.last match {
