6128c5ff4e0e347ac0ded45efba7e88e0059b288
Pasi Eronen <pasi.eronen@reaktor.com>
2018-08-09 15:35:03 +0300

    Backport uri suffix parametrisation from master branch 3c5fd09f35a6bffc121970f33267e938ae043d1d

--- scala-cas_2.12/src/main/scala/fi/vm/sade/utils/cas/CasParams.scala
+++ scala-cas_2.12/src/main/scala/fi/vm/sade/utils/cas/CasParams.scala
@@ -1,6 +1,6 @@
 package fi.vm.sade.utils.cas
 
-import org.http4s.dsl.{resolve, uri}
+import org.http4s.dsl.resolve
 import org.http4s.{ParseFailure, Uri}
 
 case class CasUser(username: String, password: String)
@@ -8,15 +8,20 @@ case class CasUser(username: String, password: String)
 case class CasService(securityUri:Uri)
 
 case class CasParams(service: CasService, user: CasUser) {
-  override def toString = service.securityUri.toString
+  override def toString: String = service.securityUri.toString
 }
 
 object CasParams {
-  def apply(service:String, username: String, password: String): CasParams ={
+  def apply(service: String, securityUriSuffix: String, username: String, password: String): CasParams ={
     Uri.fromString(ensureTrailingSlash(service)).fold(
       (e: ParseFailure) => throw new IllegalArgumentException(e),
-      (service: Uri) => CasParams(CasService(resolve(service, uri("j_spring_cas_security_check"))), CasUser(username, password)))
+      (service: Uri) => CasParams(CasService(resolve(service, Uri.fromString(securityUriSuffix).getOrElse {
+        throw new IllegalArgumentException(s"Could not parse securityUriSuffix $securityUriSuffix")
+      })), CasUser(username, password)))
   }
+
+  def apply(service: String, username: String, password: String): CasParams = apply(service = service,
+    securityUriSuffix = "j_spring_cas_security_check", username = username, password = password)
 
   private def ensureTrailingSlash(service:String) = service.last match {
     case '/' => service
