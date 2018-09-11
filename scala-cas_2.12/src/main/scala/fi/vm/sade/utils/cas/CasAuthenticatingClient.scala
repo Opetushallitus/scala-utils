2859d0ee27bf57acfbf8bbe37d8f849d14c4ee3c
Jessika Penttinen <jessika.penttinen@siili.com>
2018-09-11 12:05:49 +0300

    Kopioitu scala-ldap-client_2.12 ja scala-cas_2.12 haarasta scala-2.12

---
+++ scala-cas_2.12/src/main/scala/fi/vm/sade/utils/cas/CasAuthenticatingClient.scala
@@ -0,0 +1,90 @@
+package fi.vm.sade.utils.cas
+
+import fi.vm.sade.utils.cas.CasClient._
+import scala.collection.mutable.ListBuffer
+
+import org.http4s.{headers, _}
+import org.http4s.client.{Client, DisposableResponse}
+import org.http4s.headers.Location
+
+import scalaz.concurrent.Task
+
+
+/**
+ *  HTTP client implementation that handles CAS authentication automatically. Sessions are maintained by keeping
+ *  a central cache of session cookies per service url. If a session cookie is not found for requested service, it is obtained using
+ *  CasClient. Stale sessions are detected and refreshed automatically.
+ */
+object CasAuthenticatingClient extends Logging {
+  def apply(casClient: CasClient,
+            casParams: CasParams,
+            serviceClient: Client,
+            clientSubSystemCode: Option[String],
+            sessionCookieName: String): Client = {
+    new CasAuthenticatingClient(casClient, casParams, serviceClient, clientSubSystemCode, sessionCookieName).httpClient
+  }
+}
+
+class CasAuthenticatingClient(casClient: CasClient,
+                              casParams: CasParams,
+                              serviceClient: Client,
+                              clientSubSystemCode: Option[String],
+                              sessionCookieName: String) extends Logging {
+  lazy val httpClient = Client(
+    open = Service.lift(open),
+    shutdown = serviceClient.shutdown
+  )
+
+  private val sessions: collection.mutable.Map[CasParams, SessionCookie] = collection.mutable.Map.empty
+
+  private def open(req: Request): Task[DisposableResponse] = {
+    openWithCasSession(getCasSession(casParams), req).flatMap {
+      case resp if sessionExpired(resp.response) =>
+        logger.debug("Session for " + casParams + " expired")
+        resp.dispose.flatMap(_ => openWithCasSession(refreshSession(casParams), req))
+      case resp =>
+        Task.now(resp)
+    }
+  }
+
+  private def addHeaders(req: Request, session: SessionCookie): Request = {
+    val csrf = "CasAuthenticatingClient"
+    var list: ListBuffer[Header] = ListBuffer(headers.Cookie(Cookie(sessionCookieName, session), Cookie("CSRF", csrf)), Header("CSRF", csrf))
+    clientSubSystemCode.foreach { cssc =>
+      list += Header("clientSubSystemCode", cssc)
+    }
+    req.putHeaders(list: _*)
+  }
+
+  private def openWithCasSession(sessionIdTask: Task[SessionCookie], request: Request): Task[DisposableResponse] = {
+    sessionIdTask.flatMap { jsessionid =>
+      val requestWithHeaders = addHeaders(request, jsessionid)
+      serviceClient.open(requestWithHeaders)
+    }
+  }
+
+  private def isRedirectToLogin(resp: Response): Boolean =
+    resp.status.code == Status.Found.code && resp.headers.get(Location).exists(_.value.contains("/cas/login"))
+
+  private def sessionExpired(resp: Response): Boolean = {
+    isRedirectToLogin(resp) || resp.status.code == Status.Unauthorized.code
+}
+
+  private def getCasSession(params: CasParams): Task[SessionCookie] = {
+    synchronized(sessions.get(params)) match {
+      case None =>
+        logger.debug(s"No existing $sessionCookieName found for " + params + ", creating new")
+        refreshSession(params)
+      case Some(session) =>
+        Task.now(session)
+    }
+  }
+
+  private def refreshSession(params: CasParams): Task[SessionCookie] = {
+    casClient.fetchCasSession(params, sessionCookieName).map { session =>
+      logger.debug("Storing new jsessionid for " + params)
+      synchronized(sessions.put(params, session))
+      session
+    }
+  }
+}
---
+++ scala-cas_2.12/src/main/scala/fi/vm/sade/utils/cas/CasClient.scala
@@ -0,0 +1,138 @@
+package fi.vm.sade.utils.cas
+
+import fi.vm.sade.utils.cas.CasClient._
+import org.http4s.Status.Created
+import org.http4s._
+import org.http4s.client._
+import org.http4s.dsl._
+import org.http4s.headers.{Location, `Set-Cookie`}
+
+import scala.xml._
+import scalaz.concurrent.Task
+
+object CasClient {
+  type SessionCookie = String
+  type Username = String
+  type TGTUrl = Uri
+  type ServiceTicket = String
+}
+
+/**
+ *  Facade for establishing sessions with services protected by CAS, and also validating CAS service tickets.
+ */
+class CasClient(virkailijaLoadBalancerUrl: Uri, client: Client) {
+  import CasClient._
+
+  def this(casServer: String, client: Client) = this(Uri.fromString(casServer).toOption.get, client)
+
+  def validateServiceTicket(service: String)(serviceTicket: ServiceTicket): Task[Username] = {
+    ServiceTicketValidator.validateServiceTicket(virkailijaLoadBalancerUrl, client, service)(serviceTicket)
+  }
+
+  /**
+   *  Establishes session with the requested service by
+   *
+   *  1) getting a CAS ticket granting ticket (TGT)
+   *  2) getting a CAS service ticket
+   *  3) getting a session cookie from the service.
+   *
+   *  Returns the session that can be used for communications later.
+   */
+  def fetchCasSession(params: CasParams, sessionCookieName: String = "JSESSIONID"): Task[SessionCookie] = {
+    val serviceUri = resolve(virkailijaLoadBalancerUrl, params.service.securityUri)
+
+    for (
+      tgt <- TicketGrantingTicketClient.getTicketGrantingTicket(virkailijaLoadBalancerUrl, client, params);
+      st <- ServiceTicketClient.getServiceTicket(client, serviceUri)(tgt);
+      session <- SessionCookieClient.getSessionCookieValue(client, serviceUri, sessionCookieName)(st)
+    ) yield {
+      session
+    }
+  }
+}
+
+private[cas] object ServiceTicketValidator {
+  def validateServiceTicket(virkailijaLoadBalancerUrl: Uri, client: Client, service: String)(serviceTicket: ServiceTicket): Task[Username] = {
+    val pUri: Uri = resolve(virkailijaLoadBalancerUrl, uri("/cas/serviceValidate"))
+      .withQueryParam("ticket", serviceTicket)
+      .withQueryParam("service",service)
+
+    client.fetch(GET(pUri)) {
+      case r if r.status.isSuccess =>
+        r.as[String].map(s => Utility.trim(scala.xml.XML.loadString(s))).map {
+          case <cas:serviceResponse><cas:authenticationSuccess><cas:user>{user}</cas:user></cas:authenticationSuccess></cas:serviceResponse> =>
+            user.text
+          case authenticationFailure =>
+            throw new CasClientException(s"Service Ticket validation response decoding failed at ${service}: response body is of wrong form ($authenticationFailure)")
+        }
+      case r => r.as[String].map {
+        case body => throw new CasClientException(s"Decoding username failed at ${pUri}: CAS returned non-ok status code ${r.status.code}: $body")
+      }
+    }
+  }
+}
+
+private[cas] object ServiceTicketClient {
+  import CasClient._
+
+  def getServiceTicket(client: Client, service: Uri)(tgtUrl: TGTUrl): Task[ServiceTicket] = {
+    client.fetch[ServiceTicket](POST(tgtUrl, UrlForm("service" -> service.toString()))) {
+      case r if r.status.isSuccess => r.as[String].map {
+        case stPattern(st) => st
+        case nonSt => throw new CasClientException(s"Service Ticket decoding failed at ${tgtUrl}: response body is of wrong form ($nonSt)")
+      }
+      case r => r.as[String].map {
+        case body => throw new CasClientException(s"Service Ticket decoding failed at ${tgtUrl}: unexpected status ${r.status.code}: $body")
+      }
+    }
+  }
+
+  val stPattern = "(ST-.*)".r
+}
+
+private[cas] object TicketGrantingTicketClient extends Logging {
+  import CasClient.TGTUrl
+  val tgtPattern = "(.*TGT-.*)".r
+
+  def getTicketGrantingTicket(virkailijaLoadBalancerUrl: Uri, client: Client, params: CasParams): Task[TGTUrl] = {
+    val tgtUri: TGTUrl = resolve(virkailijaLoadBalancerUrl, uri("/cas/v1/tickets"))
+    client.fetch(POST(tgtUri, UrlForm("username" -> params.user.username, "password" -> params.user.password))) {
+      case Created(resp) =>
+        val found: TGTUrl = resp.headers.get(Location).map(_.value) match {
+          case Some(tgtPattern(tgtUrl)) =>
+            Uri.fromString(tgtUrl).fold(
+              (pf: ParseFailure) => throw new CasClientException(pf.message),
+              (tgt) => tgt
+            )
+          case Some(nontgturl) =>
+            throw new CasClientException(s"TGT decoding failed at ${tgtUri}: location header has wrong format $nontgturl")
+          case None =>
+            throw new CasClientException("TGT decoding failed at ${tgtUri}: No location header at")
+        }
+        Task.now(found)
+      case r => r.as[String].map { body =>
+        throw new CasClientException(s"TGT decoding failed at ${tgtUri}: invalid TGT creation status: ${r.status.code}: $body")
+      }
+    }
+  }
+}
+
+private[cas] object SessionCookieClient {
+  import CasClient._
+
+  def getSessionCookieValue(client: Client, service: Uri, sessionCookieName: String)(serviceTicket: ServiceTicket): Task[SessionCookie] = {
+    val sessionIdUri: Uri = service.withQueryParam("ticket", List(serviceTicket)).asInstanceOf[Uri]
+    client.fetch(GET(sessionIdUri)) {
+      case resp if resp.status.isSuccess =>
+        Task.now(resp.headers.collectFirst {
+          case `Set-Cookie`(`Set-Cookie`(cookie)) if cookie.name == sessionCookieName => cookie.content
+        }.getOrElse(throw new CasClientException(s"Decoding $sessionCookieName failed at ${sessionIdUri}: no cookie found for JSESSIONID")))
+
+      case r => r.as[String].map { body =>
+        throw new CasClientException(s"Decoding $sessionCookieName failed at ${sessionIdUri}: service returned non-ok status code ${r.status.code}: $body")
+      }
+    }
+  }
+}
+
+class CasClientException(message: String) extends RuntimeException(message)
