445e243b90d8dc9583826dc451f5edb32362dd4b
teo.mertanen <teo.mertanen@gofore.com>
2019-06-12 10:21:02 +0300

    BUG-1987 small smoke test for retry in CasClient

---
+++ scala-cas_2.12/src/test/scala/fi/vm/sade/utils/cas/CasClientSmokeTest.scala
@@ -0,0 +1,28 @@
+package fi.vm.sade.utils.cas
+
+import java.net.ConnectException
+
+import fi.vm.sade.utils.cas.CasClient.SessionCookie
+import org.http4s.client.blaze
+import org.scalatest.{FreeSpec, Matchers}
+
+import scala.util.{Failure, Try}
+
+class CasClientSmokeTest extends FreeSpec with Matchers {
+
+  val params: CasParams = CasParams("http://service", "suffix", "u", "pw")
+
+  "CasClient fetch session should fail with correct (inner) exception instead of some functional problem" in {
+    val virkailijaUrl = "testiUrl"
+    val casClient = new CasClient(virkailijaUrl, blaze.defaultClient)
+
+    val result: Try[SessionCookie] = Try(casClient.fetchCasSession(params).unsafePerformSync)
+
+    (result match {
+      case Failure(e) if e.isInstanceOf[ConnectException] && e.getMessage.equals("Connection refused") =>
+        true
+      case _ =>
+        false
+    }) shouldBe true
+  }
+}
