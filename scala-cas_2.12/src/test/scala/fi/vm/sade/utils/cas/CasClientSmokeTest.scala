3abe37e58d326a93ff510ee17e9a17a089932df2
Timo Rantalaiho <timo.rantalaiho@reaktor.com>
2019-07-09 17:19:08 +0300

    Make test more robust
    This was failing as it was connecting my
    local nginx and getting a 404 instead of a
    connection refused...

--- scala-cas_2.12/src/test/scala/fi/vm/sade/utils/cas/CasClientSmokeTest.scala
+++ scala-cas_2.12/src/test/scala/fi/vm/sade/utils/cas/CasClientSmokeTest.scala
@@ -3,6 +3,7 @@ package fi.vm.sade.utils.cas
 import java.net.ConnectException
 
 import fi.vm.sade.utils.cas.CasClient.SessionCookie
+import fi.vm.sade.tcp.PortChecker
 import org.http4s.client.blaze
 import org.scalatest.{FreeSpec, Matchers}
 
@@ -13,7 +14,7 @@ class CasClientSmokeTest extends FreeSpec with Matchers {
   val params: CasParams = CasParams("http://service", "suffix", "u", "pw")
 
   "CasClient fetch session should fail with correct (inner) exception instead of some functional problem" in {
-    val virkailijaUrl = "testiUrl"
+    val virkailijaUrl = "http://localhost:" + PortChecker.findFreeLocalPort()
     val casClient = new CasClient(virkailijaUrl, blaze.defaultClient)
 
     val result: Try[SessionCookie] = Try(casClient.fetchCasSession(params).unsafePerformSync)
