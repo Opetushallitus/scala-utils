3f3472a18d8a09135671a14fc2184d93d40f290b
Tomi Karlstedt <tomi.karlstedt@reaktor.com>
2024-11-26 12:38:48 +0200

    OPHYK-356 remove dependency tcp-utils from java-utils

--- scala-cas_2.11/src/test/scala/fi/vm/sade/utils/cas/CasClientSmokeTest.scala
+++ scala-cas_2.11/src/test/scala/fi/vm/sade/utils/cas/CasClientSmokeTest.scala
@@ -3,7 +3,7 @@ package fi.vm.sade.utils.cas
 import java.net.ConnectException
 
 import fi.vm.sade.utils.cas.CasClient.SessionCookie
-import fi.vm.sade.tcp.PortChecker
+import fi.vm.sade.utils.tcp.PortChecker
 import org.http4s.Uri
 import org.http4s.client.blaze
 import org.scalatest.{FreeSpec, Matchers}
@@ -15,7 +15,7 @@ class CasClientSmokeTest extends FreeSpec with Matchers {
   val params: CasParams = CasParams("http://service", "suffix", "u", "pw")
 
   "CasClient fetch session should fail with correct (inner) exception instead of some functional problem" in {
-    val virkailijaUrl = "http://localhost:" + PortChecker.findFreeLocalPort()
+    val virkailijaUrl = "http://localhost:" + PortChecker.findFreeLocalPort.toString
     val casClient = new CasClient(virkailijaUrl, blaze.defaultClient, "my-caller-id")
 
     val result: Try[SessionCookie] = Try(casClient.fetchCasSession(params).unsafePerformSync)
--- scala-cas_2.12/src/test/scala/fi/vm/sade/utils/cas/CasClientSmokeTest.scala
+++ scala-cas_2.12/src/test/scala/fi/vm/sade/utils/cas/CasClientSmokeTest.scala
@@ -2,7 +2,6 @@ package fi.vm.sade.utils.cas
 
 import cats.effect.IO
 import cats.effect.unsafe.IORuntime
-import fi.vm.sade.tcp.PortChecker
 import fi.vm.sade.utils.cas.CasAuthenticatingClient.DefaultSessionCookieName
 import fi.vm.sade.utils.cas.CasClient.SessionCookie
 import org.http4s.blaze.client.BlazeClientBuilder
@@ -11,7 +10,10 @@ import org.scalatest.freespec.AnyFreeSpec
 import org.scalatest.matchers.should.Matchers
 
 import scala.concurrent.ExecutionContext.global
-import scala.util.{Failure, Try}
+import scala.util.{Failure, Random, Try}
+
+import java.io.IOException
+import java.net.Socket
 
 
 class CasClientSmokeTest extends AnyFreeSpec with Matchers {
@@ -20,7 +22,7 @@ class CasClientSmokeTest extends AnyFreeSpec with Matchers {
   val params: CasParams = CasParams("http://service", "suffix", "u", "pw")
 
   "CasClient fetch session should fail with correct (inner) exception instead of some functional problem" in {
-    val virkailijaUrl = "http://localhost:" + PortChecker.findFreeLocalPort()
+    val virkailijaUrl = "http://localhost:" + findFreeLocalPort.toString
     val blazeClient = BlazeClientBuilder[IO](global).allocated.map(_._1).unsafeRunSync()
     val casClient = new CasClient(virkailijaUrl, blazeClient, "my-caller-id")
 
@@ -31,6 +33,26 @@ class CasClientSmokeTest extends AnyFreeSpec with Matchers {
         e shouldBe a[ConnectionFailure]
         e.getMessage should startWith("Error connecting to")
       case _ => fail("Should not succeed")
+    }
+  }
+
+  def isFreeLocalPort(port: Int): Boolean = {
+    try {
+      val socket = new Socket("127.0.0.1", port)
+      socket.close()
+      false
+    } catch {
+      case e:IOException => true
+    }
+  }
+
+  def findFreeLocalPort: Int = {
+    val range = 1024 to 60000
+    val port = ((range(new Random().nextInt(range length))))
+    if (isFreeLocalPort(port)) {
+      port
+    } else {
+      findFreeLocalPort
     }
   }
 }
